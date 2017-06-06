ecea834576b0c11d7bc5bf03ef234885d59a4a5d
Bang0123 <nikolajbang1995@gmail.com>
2017-06-06 12:02:21 +0200

    Added roll a ball arena

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text countText;
+    public Text WinText;
+
+    private Rigidbody _rb;
+    private float _jump = 20;
+    public float _speed;
+    private int _count;
+
+    public void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+
+    public void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        bool jump = Input.GetKeyDown(KeyCode.Space);
+
+
+        Vector3 movement = new Vector3(moveHorizontal, jump ? _jump : 0.0f, moveVertical);
+
+        _rb.AddForce(movement * _speed);
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + _count;
+        if (_count >= 12)
+        {
+            WinText.text = "You Win!!!!!";
+        }
+    }
+
+}
